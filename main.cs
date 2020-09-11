22ce799b47659e83e467c16154f81ff95ce4c402
tushit jain <tushit.jain@sgsdd-students.com>
2020-09-11 10:05:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass {
+  public static void Main (string[] args) {
+    isPrime(10);
+  }
+
+  public static void isPrime (int i){
+    int counter = 1;
+    List<int> mods = new List<int>();
+    while (counter <= i){
+      if (i % counter == 0){
+        mods.Add(counter);
+      }
+      counter++;
+      
+    }
+    int len = mods.Count;
+    foreach (int x in mods){
+      WriteLine(x);
+    }
+    if (len == 2){
+      WriteLine("{0} is a prime number" , i);
+    }
+    else{
+      WriteLine("{0} is not a prime number" , i);
+    }
+  }
+
+}
